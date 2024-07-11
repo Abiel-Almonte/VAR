715c164da9ff15fda27a9fcbf6fc0e56f54eec1b
Abiel Almonte <almonteabiel@gmail.com>
2024-07-10 23:02:31 -0400

    refactor: call function is a generator function

--- src/model.py
+++ src/model.py
@@ -1,9 +1,9 @@
 from vllm import AsyncLLMEngine, AsyncEngineArgs, SamplingParams
 from transformers import AutoTokenizer
-from collections import deque
-import uuid, asyncio
 from vectordb import CLIPCompositeIndexer
+from collections import deque
 from consts import*
+import uuid
 
 
 class vLLM_Engine():
@@ -17,96 +17,47 @@ class vLLM_Engine():
             disable_log_requests= True,
         )
 
-        self.tokens= deque(maxlen=window_size)
-        self.similarity_threshold = 0.8
-        self.special_token_count=0
+        self.tokens_window= deque(maxlen=window_size)
         self.vectorDB= CLIPCompositeIndexer()
-        self.images= []
         self.tokenizer= AutoTokenizer.from_pretrained(MODEL)
         self.llm= AsyncLLMEngine.from_engine_args(engine_args)
-    
-    async def inference(
-        self,
-        prompt: str,
-        _uuid: str,
-        temperature: float= 0.0,
-        max_tokens:int= 2048,
-        stream: bool= False
-    ):
-        
-        response_generator= self.llm.generate(
-            prompt,
-            SamplingParams(
-                temperature=temperature,
-                max_tokens=max_tokens,
-                skip_special_tokens=False,
-                spaces_between_special_tokens=False,
-            ),
-            _uuid
-        )
+        self.img_count= 0
 
-        output= await self.output_response(response_generator, stream)
+        self.retrieved_images= list()
 
-        self.special_token_count=0
-        async for text in output:
-            self.post_process(text)
-
-    async def output_response(
-        self,
-        response_generator,
-        stream: bool,
-    ):
-        if stream: return self.stream_output(response_generator)
-
-        else: return await self.static_output(response_generator) 
-                
-    async def stream_output(self, response_generator):
-        counter= 0
-        async for request_output in response_generator:
-            output= request_output.outputs[0]
-            text= output.text[counter:]
-            yield text
-            counter+= len(text)
-
-    async def static_output(self, response_generator):
-        async for request_output in response_generator: pass
-        if request_output.finished:
-            response= request_output.outputs[0].text
-            return response
-    
-    def post_process(self, text:str):
-        encoded_input= self.tokenizer(text, add_special_tokens=False).input_ids
-
-        if 32768 in encoded_input: #special token for images <|IMG|>
-
-            if self.special_token_count< 5: 
-
-                self.insert_image(
-                    self.tokenizer.decode(list(self.tokens), skip_special_tokens=True))
-                self.special_token_count+= 1
-
-        else: 
-            print(text, end='')
-            self.tokens.extend(encoded_input)
-        
-    def insert_image(self, text:str):
-        print("\n===================\n IMAGE_HERE \n")
-        self.images.append(self.vectorDB.search(text))
-        print(f"Caption: {self.images[-1][0]['caption']}\n===================\n")
-        
-    def __call__(
+    async def __call__(
         self,
         question: str,
     ):
         prompt= IMG_PROMPT.format(question= question)
-        asyncio.run(self.inference(prompt, str(uuid.uuid4()), stream=True))
-        
+        response_generator= self.llm.generate(
+            prompt,
+            SamplingParams(
+                temperature=TEMP,
+                max_tokens=MAX_TOKENS,
+                skip_special_tokens=False,
+                spaces_between_special_tokens=False,
+            ),
+            str(uuid.uuid4())
+        )
 
-## TEST
-if __name__ == '__main__':
-    engine= vLLM_Engine()
-    while 1:
-        _in = input('Enter prompt: \n')
-        if _in != 'end':
-            engine(_in)
-        else: break
+        counter= 0
+        async for request_output in response_generator:
+            output= request_output.outputs[0]
+            text= output.text[counter:]
+            counter+= len(text)
+
+            encoded_input= self.tokenizer(text, add_special_tokens=False).input_ids
+
+            if IMG_TOKEN in encoded_input: #<|IMG|>
+
+                decoded_query= self.tokenizer.decode(
+                    list(self.tokens_window),
+                    skip_special_tokens=True)
+                
+                self.retrieved_images.append(self.vectorDB.search(decoded_query))
+                yield "<|IMG|>"
+
+            else:
+                self.tokens_window.extend(encoded_input)
+                yield text
